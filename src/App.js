934a2b0f20147127994ff73d96fa419c22c29e60
purva10 <purvaoyal10@gmail.com>
2022-05-01 21:33:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import About from './components/About';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <About/>
+     
+    </div>
+  );
+}
+
+export default App;
